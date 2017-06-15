2351d8b805f27192e8a22882d6f332448614b8e3
Riku Pepponen <riku.pepponen@gmail.com>
2017-06-16 01:16:06 +0300

    Initial commit

---
+++ app/src/main/java/exxxample/xtremedice/MainActivity.java
@@ -0,0 +1,13 @@
+package exxxample.xtremedice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
